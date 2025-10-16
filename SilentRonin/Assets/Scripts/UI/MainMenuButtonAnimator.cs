f4c4376056e275101cc2538968c24758cac647ee
manxuan8231 <lydangxuanman05@gmail.com>
2025-10-16 18:12:35 +0700

    mainmenu

---
+++ SilentRonin/Assets/Scripts/UI/MainMenuButtonAnimator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuButtonAnimator : MonoBehaviour
+{
+    public Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Khi rê chuột vào
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetTrigger("Hover");
+    }
+
+    // Khi rê chuột ra ngoài
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetTrigger("Normal");
+    }
+
+    // Khi click
+    public void OnPointerClick()
+    {
+        anim.SetTrigger("Click");
+    }
+}
---
+++ SilentRonin/Assets/Scripts/UI/MainMenuUIHandler.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUIHandler : MonoBehaviour
+{
+    public GameObject panelQuit;// Panel xác nhận thoát game
+    [Header("UI MainMenu")]
+    public GameObject button;
+    public GameObject imageTitle;
+    public GameObject imageGround;
+
+    public void OnStartGame()
+    {
+        SceneManager.LoadScene("Level_01"); 
+    }
+
+    // Gọi khi bấm "Options"
+    public void OnOptions()
+    {
+        Debug.Log("Options menu opened");
+        // TODO: mở panel cài đặt
+    }
+
+    // Gọi khi bấm "Achievements"
+    public void OnAchievements()
+    {
+        Debug.Log("Achievements opened");
+        // TODO: mở panel thành tựu
+    }
+
+    // Gọi khi bấm "Quit Game"
+    public void OpenPanelQuitGame()
+    {
+        panelQuit.SetActive(true);
+        imageTitle.SetActive(false);
+        imageGround.SetActive(false);
+        button.SetActive(false);
+    } 
+    public void YesQuitGame()
+    {
+        Application.Quit();
+       
+    }
+    public void NoQuitGame()
+    {
+       
+        imageTitle.SetActive(true);
+        imageGround.SetActive(true);
+        button.SetActive(true);
+        panelQuit.SetActive(false);
+    }
+}
